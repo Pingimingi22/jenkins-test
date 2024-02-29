ee51b341b54b087a2b4de5f608a59ecc62215691
Daniel Hadzajlic <daniel@jig.space>
2024-02-29 15:26:06 +1100

    Fixed error

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        asdasd
+        Debug.Log("Hihi");
     }
 
     // Update is called once per frame
